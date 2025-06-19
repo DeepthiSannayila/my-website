e2c566d056421f990754fdf86a6b62d1c69af819
Deepthi <deepthisannayila@gmail.com>
2025-06-19 14:42:48 +0530

    changes

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('/about.html')
 def about():
     return render_template('about.html')
 
-@app.route('/services')
+@app.route('/services.html')
 def services():
     return render_template('services.html')
 
-@app.route('/contact')
+@app.route('/contact.html')
 def contact():
     return render_template('contact.html')
 
--- requirements.py
+++ requirements.py
@@ -1,1 +1,2 @@
-Flask==3.1.1
+# To install Flask, use the following command in your terminal:
+# pip install Flask==3.1.1
